9e4d3b5daef69cb05a20918794957919e9582a7e
enokawa <enokawa00@gmail.com>
2022-11-03 00:58:35 +0900

    Init execute_statement

--- src/layers/db/db.py
+++ src/layers/db/db.py
@@ -1,5 +1,25 @@
+import json
+import os
+
+import boto3
+
+
+client = boto3.client('rds-data')
+RDS_ARN = os.getenv('RDS_ARN')
+SECRET_ARN = os.getenv('SECRET_ARN')
+DATABASE = os.getenv('DATABASE_NAME')
+
+
 def execute_statement() -> list[dict]:
-  return [{
-    'name': 'john',
-    'email': 'john@aurora-data-api-sample.dev'
-  }]
+  response = client.execute_statement(
+    resourceArn=RDS_ARN,
+    secretArn=SECRET_ARN,
+    sql='SELECT `name`, `email` FROM `users`;',
+    database=DATABASE,
+    includeResultMetadata=True,
+    formatRecordsAs='JSON'
+  )
+  
+  formated = response['formattedRecords']
+
+  return json.loads(formated)
