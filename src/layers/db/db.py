3159d67fd430d9aad62fb3c47cd85c3af6311710
enokawa <enokawa00@gmail.com>
2022-11-03 00:17:25 +0900

    init execute_statement

--- src/layers/db/db.py
+++ src/layers/db/db.py
@@ -1,2 +1,5 @@
-def test():
-  return 'db'
+def execute_statement() -> list[dict]:
+  return [{
+    'name': 'john',
+    'email': 'john@aurora-data-api-sample.dev'
+  }]
--- src/tests/unit/test_db.py
+++ src/tests/unit/test_db.py
@@ -1,10 +1,9 @@
-import json
-
-import pytest
-
 from src.layers.db import db
 
-def test_db():
-  ret = db.test()
-  
-  assert ret
+def test_execute_statement():
+  ret = db.execute_statement()
+
+  assert ret == [{
+    'name': 'john',
+    'email': 'john@aurora-data-api-sample.dev'
+  }]
